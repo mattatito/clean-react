ad132c3739759a151af9f686bc0d7cbbefe7291f
Matheus Geiser <matheusgeiser@gmail.com>
2024-12-30 00:05:23 -0300

    feat: ensure RemoteAuthentication calls HttpPostClient with correct body

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,3 +1,4 @@
+import { mockAuthentication } from '../../../domain/test/mock-authentication'
 import { HttpPostClientSpy } from '../../test/mock-http-client'
 import { RemoteAuthentication } from './remote-authentication'
 import faker from 'faker'
@@ -20,7 +21,14 @@ describe('RemoteAuthentication', () => {
     test('Should call HttpClient with correct URL', async () => {
         const url = faker.internet.url()
         const { sut, httpPostClientSpy } = makeSut(url)
-        sut.auth()
+        sut.auth(mockAuthentication())
         expect(httpPostClientSpy.url).toBe(url)
+    })
+
+    test('Should call HttpClient with correct body', async () => {
+        const { sut, httpPostClientSpy } = makeSut()
+        const authenticationParams = mockAuthentication();
+        sut.auth(authenticationParams)
+        expect(httpPostClientSpy.body).toEqual(authenticationParams)
     })
 });
